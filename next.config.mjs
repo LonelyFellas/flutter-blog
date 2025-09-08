30f30f1a30fc49f4f36bed96502316d5571e9b18
darwish <289582304@qq.com>
2025-09-08 22:12:48 +0800

    feat: fist init project

---
+++ next.config.mjs
@@ -0,0 +1,13 @@
+import nextra from 'nextra'
+ 
+// Set up Nextra with its configuration
+const withNextra = nextra({
+  // ... Add Nextra-specific options here
+  // theme: "blog"
+
+})
+ 
+// Export the final Next.js config with Nextra included
+export default withNextra({
+  // ... Add regular Next.js options here
+})
